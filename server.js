60281aa95f8a03881e65daf7c715df247486edb3
iamfrandard <danielchalasrd@gmail.com>
2023-11-12 23:13:59 -0400

    commit

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ require("./app/routes/appointment.routes")(app);
 require("./app/routes/appointmentClient.routes")(app);
 require("./app/routes/appointmentWorkshop.routes")(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server - READY");
 });
