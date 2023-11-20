8d8ed1cbc450cd79b1dc145e110ecce70646c26c
iamfrandard <danielchalasrd@gmail.com>
2023-11-20 01:33:18 -0400

    commit

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -123,7 +123,7 @@ exports.signupE = (req, res) => {
     password: bcrypt.hashSync(req.body.inputPasswordW, 8),
     idNumber: req.body.inputWorkshopID,
     locations: locations,
-    vehicleBrand: { name: brands },
+    vehicleBrand: brands.map((brand) => ({ name: brand })),
     vehicleService: services,
   });
 
